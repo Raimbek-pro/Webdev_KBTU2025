d84108b67e871adca735c0715f810da3f7271524
ssss1131 <sauletmardan20@gmail.com>
2025-04-06 19:14:02 +0500

    feat: add JWT register login logout and getting information about user by his jwt token

---
+++ horizon-backend/users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Хеширование пароля
+        user.save()
+        return user
---
+++ horizon-backend/users/views.py
@@ -0,0 +1,85 @@
+import jwt, datetime
+import os
+
+from decouple import Config, RepositoryEnv
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
+
+from users.models import User
+from users.serializers import UserSerializer
+from django.core.management.utils import get_random_secret_key
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+secrets_file = os.path.join(BASE_DIR, 'secrets.env')
+config = Config(RepositoryEnv(secrets_file))
+
+SECRET_KEY = config('SECRET_KEY')
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        print(get_random_secret_key())
+        return Response(serializer.data)
+
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = User.objects.filter(username=username).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.now(datetime.UTC)
+        }
+
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
+
+
+class UserView(APIView):
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
